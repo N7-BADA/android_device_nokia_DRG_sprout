2a5964f3b51d8aee268fb7b51d6e81af652dc4b4
Siddharth Pranjale <sidpranjale127@protonmail.com>
2020-08-03 09:53:04 +0530

    DRG: repull and clean up some more configs
    libvndfwk_detect_jni.qti was duplicated

--- device.mk
+++ device.mk
@@ -160,19 +160,7 @@ PRODUCT_BOOT_JARS += \
 # Framework Detect
 PRODUCT_PACKAGES += \
 	libqti_vndfwk_detect \
-	libqti_vndfwk_detect.vendor \
-	libvndfwk_detect_jni.qti \
-	libvndfwk_detect_jni.qti.vendor
-
-# GPS
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/configs/gps/apdr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/apdr.conf \
-	$(LOCAL_PATH)/configs/gps/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
-	$(LOCAL_PATH)/configs/gps/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf \
-	$(LOCAL_PATH)/configs/gps/izat.conf:$(TARGET_COPY_OUT_VENDOR)/etc/izat.conf \
-	$(LOCAL_PATH)/configs/gps/lowi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/lowi.conf \
-	$(LOCAL_PATH)/configs/gps/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
-	$(LOCAL_PATH)/configs/gps/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
+	libqti_vndfwk_detect.vendor
 
 # Health
 PRODUCT_PACKAGES += \
